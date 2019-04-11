12bfb9d5d03355c0b24a4a5b5762d51c450898d3
Sam Weaver <samson_dw@hotmail.com>
2019-04-11 10:39:33 -0400

    Updated to use callbacks

--- mRotaryEncoder.cpp
+++ mRotaryEncoder.cpp
@@ -3,29 +3,26 @@
 
 
 mRotaryEncoder::mRotaryEncoder(PinName pinA, PinName pinB, PinName pinSW, PinMode pullMode, int debounceTime_us) {
-    m_pinA = new PinDetect(pinA);                      // interrrupts on pinA
-    m_pinB = new DigitalIn(pinB);                      // only digitalIn for pinB
+    m_pinA = new PinDetect(pinA);                           // interrrupts on pinA
+    m_pinB = new DigitalIn(pinB);                           // only digitalIn for pinB
 
     //set pins with internal PullUP-default
     m_pinA->mode(pullMode);
     m_pinB->mode(pullMode);
 
     // attach interrrupts on pinA
-    m_pinA->attach_asserted(this, &mRotaryEncoder::rise);
-    m_pinA->attach_deasserted(this, &mRotaryEncoder::fall);
-    
+    m_pinA->attach_asserted(callback(this, &mRotaryEncoder::rise));
+    m_pinA->attach_deasserted(callback(this, &mRotaryEncoder::fall));
     //start sampling pinA
-    m_pinA->setSampleFrequency(debounceTime_us);                  // Start timers an Defaults debounce time.
+    m_pinA->setSampleFrequency(debounceTime_us);            // Start timers an Defaults debounce time.
 
     // Switch on pinSW
-    m_pinSW = new PinDetect(pinSW);                 // interrupt on press switch
+    m_pinSW = new PinDetect(pinSW);                         // interrupt on press switch
     m_pinSW->mode(pullMode);
-    
-    m_pinSW->setSampleFrequency(debounceTime_us);                  // Start timers an Defaults debounce time.
-
+    m_pinSW->setAssertValue(0);
+    m_pinSW->setSampleFrequency(debounceTime_us);           // Start timers an Defaults debounce time.
 
     m_position = 0;
-
     m_debounceTime_us = debounceTime_us;
 }
 
@@ -39,40 +36,37 @@ int mRotaryEncoder::Get(void) {
     return m_position;
 }
 
-
-
 void mRotaryEncoder::Set(int value) {
     m_position = value;
 }
 
-
 void mRotaryEncoder::fall(void) {
-    // debouncing does PinDetect for us
-    //pinA still low?
+    // PinDetect does debouncing for us
+    // Is pinA still low?
     if (*m_pinA == 0) {
         if (*m_pinB == 1) {
             m_position++;
-            rotCWIsr.call();
+            if (rotCWIsr) rotCWIsr();
         } else {
             m_position--;
-            rotCCWIsr.call();
+            if (rotCCWIsr) rotCCWIsr();
         }
-    rotIsr.call();                        // call the isr for rotation
+        if (rotIsr) rotIsr();          // call the isr for rotation
     }
 }
 
 void mRotaryEncoder::rise(void) {
-    //PinDetect does debouncing
-    //pinA still high?
+    // PinDetect does debouncing for us
+    // Is pinA still high?
     if (*m_pinA == 1) {
         if (*m_pinB == 1) {
             m_position--;
-            rotCCWIsr.call();
+            if (rotCCWIsr) rotCCWIsr();
         } else {
             m_position++;
-            rotCWIsr.call();
+            if (rotCWIsr) rotCWIsr();
         }
-    rotIsr.call();                        // call the isr for rotation
+        if (rotIsr) rotIsr();          // call the isr for rotation
     }
 }
 
