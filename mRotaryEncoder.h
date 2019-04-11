1f9d4aab0a2b8eed59b2b6d6f30172656639e488
Sam Weaver <samson_dw@hotmail.com>
2019-04-11 10:36:27 -0400

    Added additional attach points
    Added additional attach points:
    attachSWReleased, attachSWHeld, & attachSWHoldReleased

--- mRotaryEncoder.h
+++ mRotaryEncoder.h
@@ -30,10 +30,10 @@
  *
  * changelog:
  *
- * 09. Nov. 2010
- *     First version published Thomas Raab raabinator
+ * 09.Nov.2010 - First version published Thomas Raab raabinator
  * 26.11.2010 extended by charly - pushbutton, pullmode, debounce, callback-system
  * Feb2011 Changes InterruptIn to PinDetect which does the debounce of mechanical switches
+ * Apr2019 extended by samsondw - attachSWReleased, attachSWHeld, attachSWHoldReleased
  *
  */
 class mRotaryEncoder {
@@ -49,23 +49,18 @@ public:
     mRotaryEncoder(PinName pinA, PinName pinB, PinName pinSW, PinMode pullMode=PullUp, int debounceTime_us=1000);
 
     /** destroy object
-    *
     */
     ~mRotaryEncoder();
 
+
     /** Get the actual value of the rotary position
-    *
     * @return position int value of position
     */
     int Get(void);
-    inline operator int() {
-        return Get();
-    }
+    inline operator int() { return Get(); }
 
     /** Set the current position value
-    *
     * @param value the new position to set
-    *
     */
     void Set(int value);
     inline mRotaryEncoder& operator= ( int  value ) {
@@ -73,53 +68,97 @@ public:
         return *this;
     }
 
+
     /** attach a function to be called when switch is pressed
-    *
     * keep this function short, as no interrrupts can occour within
-    *
     * @param fptr Pointer to callback-function
     */
-    void attachSW(void (*fptr)(void)) {
-        m_pinSW->attach_deasserted(fptr);
-    }
+    void attachSW(void (*fptr)(void)) { m_pinSW->attach_asserted(fptr); }
 
     template<typename T>
     /** attach an object member function to be called when switch is pressed
-    *
     * @param tptr pointer to object
     * @param mprt pointer ro member function
-    *
     */
     void attachSW(T* tptr, void (T::*mptr)(void)) {
+        if ((mptr != NULL) && (tptr != NULL)) {
+            m_pinSW->attach_asserted(tptr, mptr);
+        }
+    }
+
+
+    /** attach a function to be called when switch is released
+    * keep this function short, as no interrrupts can occour within
+    * @param fptr Pointer to callback-function
+    */
+    void attachSWReleased(void (*fptr)(void)) { m_pinSW->attach_deasserted(fptr); }
+
+    template<typename T>
+    /** attach an object member function to be called when switch is released
+    * @param tptr pointer to object
+    * @param mprt pointer ro member function
+    */
+    void attachSWReleased(T* tptr, void (T::*mptr)(void)) {
         if ((mptr != NULL) && (tptr != NULL)) {
             m_pinSW->attach_deasserted(tptr, mptr);
         }
     }
 
-    /**  callback-System for rotation of shaft
-    *
-    *  attach a function to be called when the shaft is rotated
-    *  keep this function short, as no interrrupts can occour within
-    *
-    * @param fprt Pointer to callback-function
+
+    /** attach a function to be called when switch is held
+    * keep this function short, as no interrrupts can occour within
+    * @param fptr Pointer to callback-function
     */
-    void attachROT(void (*fptr)(void)) {
-        rotIsr.attach(fptr);
+    void attachSWHeld(void (*fptr)(void)) { m_pinSW->attach_asserted_held(fptr); }
+
+    template<typename T>
+    /** attach an object member function to be called when switch is held
+    * @param tptr pointer to object
+    * @param mprt pointer ro member function
+    */
+    void attachSWHeld(T* tptr, void (T::*mptr)(void)) {
+        if ((mptr != NULL) && (tptr != NULL)) {
+            m_pinSW->attach_asserted_held(tptr, mptr);
+        }
     }
 
+
+    /** attach a function to be called when switch hold is released
+    * keep this function short, as no interrrupts can occour within
+    * @param fptr Pointer to callback-function
+    */
+    void attachSWHoldReleased(void (*fptr)(void)) { m_pinSW->attach_deasserted_held(fptr); }
+
+    template<typename T>
+    /** attach an object member function to be called when switch hold is released
+    * @param tptr pointer to object
+    * @param mprt pointer ro member function
+    */
+    void attachSWHoldReleased(T* tptr, void (T::*mptr)(void)) {
+        if ((mptr != NULL) && (tptr != NULL)) {
+            m_pinSW->attach_deasserted_held(tptr, mptr);
+        }
+    }
+
+
+    /**  callback-System for rotation of shaft
+    *  attach a function to be called when the shaft is rotated
+    *  keep this function short, as no interrrupts can occour within
+    * @param fprt Pointer to callback-function
+    */
+    void attachROT(void (*fptr)(void)) { rotIsr = callback(fptr); }
 
     template<typename T>
     /** attach an object member function to be called when shaft is rotated
-    *
     * @param tptr pointer to object
     * @param mprt pointer ro member function
-    *
     */
     void attachROT(T* tptr, void (T::*mptr)(void)) {
         if ((mptr != NULL) && (tptr != NULL)) {
-            rotIsr.attach(tptr, mptr);
+            rotIsr = callback(tptr, mptr);
         }
     }
+
 
    /**  callback-System for rotation of shaft CW
     *
@@ -128,10 +167,7 @@ public:
     *
     * @param fprt Pointer to callback-function
     */
-    void attachROTCW(void (*fptr)(void)) {
-        rotCWIsr.attach(fptr);
-    }
-
+    void attachROTCW(void (*fptr)(void)) { rotCWIsr = callback(fptr); }
 
     template<typename T>
     /** attach an object member function to be called when shaft is rotated clockwise
@@ -142,9 +178,10 @@ public:
     */
     void attachROTCW(T* tptr, void (T::*mptr)(void)) {
         if ((mptr != NULL) && (tptr != NULL)) {
-            rotCWIsr.attach(tptr, mptr);
+            rotCWIsr = callback(tptr, mptr);
         }
     }
+
 
    /**  callback-System for rotation of shaft CCW
     *
@@ -153,10 +190,7 @@ public:
     *
     * @param fprt Pointer to callback-function
     */
-    void attachROTCCW(void (*fptr)(void)) {
-        rotCCWIsr.attach(fptr);
-    }
-
+    void attachROTCCW(void (*fptr)(void)) { rotCCWIsr = callback(fptr); }
 
     template<typename T>
     /** attach an object member function to be called when shaft is rotated CCW
@@ -167,7 +201,7 @@ public:
     */
     void attachROTCCW(T* tptr, void (T::*mptr)(void)) {
         if ((mptr != NULL) && (tptr != NULL)) {
-            rotCCWIsr.attach(tptr, mptr);
+            rotCCWIsr = callback(tptr, mptr);
         }
     }    
 
@@ -177,7 +211,6 @@ private:
     volatile int    m_position;
 
     int             m_debounceTime_us;
-
 
     PinDetect       *m_pinSW;
 
@@ -192,17 +225,16 @@ protected:
     /**
      * rotated any direction
     */
-    FunctionPointer rotIsr;
+    Callback<void()> rotIsr;
     /**
      * clockwise rotated
     */
-    FunctionPointer rotCWIsr;
+    Callback<void()> rotCWIsr;
 
     /**
      * counterclockwise rotated
     */    
-    FunctionPointer rotCCWIsr;
-
+    Callback<void()> rotCCWIsr;
 
 };
 
